b25e1c95ebe901d0a3c115829abdb3e127063d74
Shanti swarup <36096058+sam-94@users.noreply.github.com>
2018-11-20 18:43:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,69 +17,76 @@ var currentFrame;
 
 
 function animate() {
-var point = points[currentFrame++];
-draw(point.x, point.y);
+    var point = points[currentFrame++];
+    draw(point.x, point.y);
 
-// refire the timer until out-of-points
-if (currentFrame < points.length) {
-  timer = setTimeout(animate, 1000 / 60);
-}
+    // refire the timer until out-of-points
+    if (currentFrame < points.length) {
+        timer = setTimeout(animate, 1000 / 60);
+    }
 }
 
 function linePoints(x1, y1, x2, y2, frames) {
-var dx = x2 - x1;
-var dy = y2 - y1;
-var length = Math.sqrt(dx * dx + dy * dy);
-var incrementX = dx / frames;
-var incrementY = dy / frames;
-var a = new Array();
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+    var length = Math.sqrt(dx * dx + dy * dy);
+    var incrementX = dx / frames;
+    var incrementY = dy / frames;
+    var a = new Array();
 
-a.push({
-  x: x1,
-  y: y1
-});
-for (var frame = 0; frame < frames - 1; frame++) {
-  a.push({
-      x: x1 + (incrementX * frame),
-      y: y1 + (incrementY * frame)
-  });
+    a.push({
+        x: x1,
+        y: y1
+    });
+    for (var frame = 0; frame < frames - 1; frame++) {
+        a.push({
+            x: x1 + (incrementX * frame),
+            y: y1 + (incrementY * frame)
+        });
+    }
+    a.push({
+        x: x2,
+        y: y2
+    });
+    return (a);
 }
-a.push({
-  x: x2,
-  y: y2
-});
-return (a);
-}
-var colorArray = [
-  '#ffaa33',
-  '#99ffaa',
-  '#00ff00',
-  '#ff1100',
-];
+ var colorArray = [
+        '#ffaa33',
+        '#99ffaa',
+        '#00ff00',
+        '#ff1100',
+  ];
 function draw(x, y) {
-ctx.clearRect(0, 0, canvas.width, canvas.height);
-ctx.beginPath();
-ctx.font = "30px arial";
-ctx.fillStyle = colorArray[Math.floor(Math.random() * colorArray.length)];
-ctx.fillText("Oh Hello", x,y);
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.font = "30px arial";
+    ctx.fillStyle = colorArray[Math.floor(Math.random() * colorArray.length)];
+    ctx.fillText("Oh Hello", x,y);
 }
 
 function handleMouseMove(e) {
-mouseX = parseInt(e.clientX - offsetX);
-mouseY = parseInt(e.clientY - offsetY);
+    mouseX = parseInt(e.clientX - offsetX);
+    mouseY = parseInt(e.clientY - offsetY);
 
-// Put your mousedown stuff here
-points = linePoints(currentX, currentY, mouseX, mouseY, frameCount);
-currentFrame = 0;
-currentX = mouseX;
-currentY = mouseY;
-animate();
+    // Put your mousedown stuff here
+    points = linePoints(currentX, currentY, mouseX, mouseY, frameCount);
+    currentFrame = 0;
+    currentX = mouseX;
+    currentY = mouseY;
+    animate();
 }
 
 $("#canvas").mousemove(function (e) {
-handleMouseMove(e);
+    handleMouseMove(e);
 });
-
+$("#canvas").mousedown(function () {
+  var a = Math.random() * innerWidth;
+   var b = Math.random() * innerHeight;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.font = "bold 130px arial";
+    ctx.fillStyle = colorArray[Math.floor(Math.random() * colorArray.length)];;
+    ctx.fillText("BAMMER !", a,b);
+});
 
 draw(0, 0);
